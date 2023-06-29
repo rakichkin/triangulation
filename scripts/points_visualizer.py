f0ad63a83e21e91702d9c7cd5e795304f237aa87
Igor-Rakitskii <rakichkin@gmail.com>
2023-06-29 17:30:49 +0300

    Добавлено отображение статичных точек, небольшой рефакторинг

--- scripts/points_visualizer.py
+++ scripts/points_visualizer.py
@@ -7,49 +7,18 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-
-def main():
-    src_point_coords = deserialize_json('.\\data\\real_moving_point_coords.json')
-    unfiltered_triang_point_coord = deserialize_json('.\\data\\unfiltered_triangulation.json')
-    kalman_triang_point_coord = deserialize_json('.\\data\\kalman_triangulation.json')
-    mkarlo_triang_point_coord = deserialize_json('.\\data\\monte_carlo_triangulation.json')
+def draw(subplot, points, title):
+    subplot.set_xlim(0, 1920)
+    subplot.set_ylim(0, 1080)
     
-    figure, subplots = plt.subplots(2, 2, figsize=(19.2, 10.8), dpi=100)
+    static_points = config['static_points']
+    for point in static_points:
+        subplot.plot(point[0], point[1], marker="o", markersize=6, markeredgecolor="blue", markerfacecolor="blue")
     
-    subplots[0, 0].set_xlim(0, 1920)
-    subplots[0, 0].set_ylim(0, 1080)
-    for point in src_point_coords:
-        subplots[0, 0].plot(point['x'], point['y'], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
-    subplots[0, 0].set_title("Raw points")
-    subplots[0,0].grid(True)
-    
-    subplots[0, 1].set_xlim(0, 1920)
-    subplots[0, 1].set_ylim(0, 1080)
-    for point in unfiltered_triang_point_coord:
-        subplots[0, 1].plot(point['x'], point['y'], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
-    subplots[0, 1].set_title("Triangulated points")
-    subplots[0,1].grid(True)
-    
-    subplots[1, 0].set_xlim(0, 1920)
-    subplots[1, 0].set_ylim(0, 1080)
-    for point in kalman_triang_point_coord:
-        subplots[1, 0].plot(point['x'], point['y'], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
-    subplots[1, 0].set_title("Kalman Filter")
-    subplots[1, 0].grid(True)
-
-    for point in mkarlo_triang_point_coord:
-        subplots[1, 1].plot(point['x'], point['y'], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
-    subplots[1, 1].set_title("Monte-Carlo filter")
-    subplots[1,1].grid(True)
-
-
-    # figure.show()
-    # plt.waitforbuttonpress()
-    
-    img_name = f'data\\imgs\\{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.png'
-    figure.savefig(img_name)
-
-    print_config_values_and_save(img_name)
+    for point in points:
+        subplot.plot(point['x'], point['y'], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
+    subplot.set_title(title)
+    subplot.grid(True)
 
 def print_config_values_and_save(img_name):
     with open('config.json', 'r') as file:
@@ -67,6 +36,31 @@ def deserialize_json(file_path:str):
         data_str = file.read()
     data = json.loads(data_str)
     return data
+
+def read_config():
+    with open('config.json', 'r') as file:
+        config_str = file.read()
+        return json.loads(config_str)
+    
+config = read_config()
+
+def main():
+    src_point_coords = deserialize_json('.\\data\\real_moving_point_coords.json')
+    unfiltered_triang_point_coord = deserialize_json('.\\data\\unfiltered_triangulation.json')
+    kalman_triang_point_coord = deserialize_json('.\\data\\kalman_triangulation.json')
+    mkarlo_triang_point_coord = deserialize_json('.\\data\\monte_carlo_triangulation.json')
+
+    figure, subplots = plt.subplots(2, 2, figsize=(19.2, 10.8), dpi=100)
+    
+    draw(subplots[0, 0], src_point_coords, 'Raw points')
+    draw(subplots[0, 1], unfiltered_triang_point_coord, 'Triangulated points')
+    draw(subplots[1, 0], kalman_triang_point_coord, 'Kalman Filter')
+    draw(subplots[1, 1], mkarlo_triang_point_coord, 'Monte-Carlo filter')
+
+    img_name = f'data\\imgs\\{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.png'
+    figure.savefig(img_name)
+
+    print_config_values_and_save(img_name)
 
 if __name__ == "__main__":
     main()
